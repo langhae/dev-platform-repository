a6a9fe81c597a714acec5d9b0dd000664bd04fef
langhae <103482254+langhae@users.noreply.github.com>
2022-06-25 23:02:48 +0900

    Update main.tf

--- CloudFront/main.tf
+++ CloudFront/main.tf
@@ -31,21 +31,42 @@ resource "aws_cloudfront_distribution" "this" {
   origin {
     domain_name = aws_s3_bucket.this.bucket_regional_domain_name
     origin_id   = local.s3_origin_id
+    origin_path = "/iot"
       
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.this.cloudfront_access_identity_path
     }
   }
-
+  
+  aliases             = "d604721fxaaqy9.cloudfront.net"
   enabled             = true
   is_ipv6_enabled     = true
   comment             = "my distribution"
   
   default_cache_behavior {
-    allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    allowed_methods = ["GET", "HEAD"]
     cached_methods  = ["GET", "HEAD"]
-    cache_policy_id = local.s3_origin_id
-    path_pattern    = "*"
+    viewer_protocol_policy = "allow-all"
+    target_origin_id = local.s3_origin_id
+    cache_policy_id = local.cache_policy_id
+  }
+  
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+  
+  viewer_certificate {
+    cloudfront_default_certificate = false
+    acm_certificate_arn = "arn:aws:acm:us-east-1:940168446867:certificate/7ce5554b-b489-4de5-a393-02ee0d00c9e1"
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method = "sni-only"
   }
 
 }
@@ -82,5 +103,6 @@ resource "aws_s3_bucket_acl" "b_acl" {
 }
 
 locals {
+  cache_policy_id = aws_cloudfront_cache_policy.this.id
   s3_origin_id = "myS3Origin"
 }
